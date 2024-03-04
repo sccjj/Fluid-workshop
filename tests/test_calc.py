c3c47b96d213a73aa7f31c8873b287c926e44a0e
Chris Jackson <sccjj@leeds.ac.uk>
2024-03-04 13:39:17 +0000

    add test calc

---
+++ tests/test_calc.py
@@ -0,0 +1,9 @@
+
+def test_negatives():
+    assert adivb(-10,-5) == 2, "two neg return pos "
+
+#assert adivb(-9,3) == -3, "one pos one neg =one neg"
+#assert adivb(35,7) >0, "two pos = one pos"
+#assert isinstance(adivb(5,10), float), "minor by l;arger should be float"
+# assert type(adivb(5,10)) == type(1.0), "minor b larger should be float"
+#assert adivb(10,3) == pytest.approx(3.33, 0.01), "10/3 should be 3.333"
