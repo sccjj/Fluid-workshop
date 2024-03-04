406beb5eef8ba52687eeac787a6008583c0bebef
Chris Jackson <sccjj@leeds.ac.uk>
2024-03-04 14:07:48 +0000

    more tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,9 +1,22 @@
 import functions.calc as fc
-def test_negatives():
-    assert fc.adivb(-10,-5) == 2, "two neg return pos "
+import pytest
 
-#assert adivb(-9,3) == -3, "one pos one neg =one neg"
-#assert adivb(35,7) >0, "two pos = one pos" 
-#assert isinstance(adivb(5,10), float), "minor by l;arger should be float"
-# assert type(adivb(5,10)) == type(1.0), "minor b larger should be float"
-#assert adivb(10,3) == pytest.approx(3.33, 0.01), "10/3 should be 3.333"
+
+def test_negatives():
+    assert fc.adivb(-10, -5) == 2, "two neg return pos "
+
+
+def test_pos_neg():
+    assert fc.adivb(-9, 3) == -3, "one pos one neg =one neg"
+
+
+def test_pos():
+    assert fc.adivb(35, 7) > 0, "two pos = one pos"
+
+
+def test_type():
+    assert isinstance(fc.adivb(5, 10), float), "minor by l;arger should be float"
+
+
+def test_rational():
+    assert fc.adivb(10, 3) == pytest.approx(3.33, 0.01), "10/3 should be 3.333"
